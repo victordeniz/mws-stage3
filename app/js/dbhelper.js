cda4db6effce3d69268758b7cc0a0a0e786ba754
Victor Deniz <victor@moodle.com>
2018-08-29 22:44:37 +0100

    Final version

--- app/js/dbhelper.js
+++ app/js/dbhelper.js
@@ -209,7 +209,7 @@ class DBHelper {
 
     static getStoredObjectById(table, idx, id) {
         return this.dbPromise()
-            .then(function(db) {
+            .then(function (db) {
                 if (!db) return;
 
                 const store = db.transaction(table).objectStore(table);
@@ -229,7 +229,7 @@ class DBHelper {
                         let tx = db.transaction("reviews", "readwrite");
                         const store = tx.objectStore("reviews");
                         if (Array.isArray(reviews)) {
-                            reviews.forEach(function(review) {
+                            reviews.forEach(function (review) {
                                 store.put(review);
                             });
                         } else {
@@ -263,44 +263,47 @@ class DBHelper {
             "comments": review.comments,
             "restaurant_id": parseInt(review.restaurant_id)
         };
-        var fetch_options = {
-            method: "POST",
-            body: JSON.stringify(reviewSend)/*,
-            headers: new Headers({
-                "Content-Type": "application/json"
-            })*/
-        };
-        fetch(`${DBHelper. DATABASE_URL}reviews/`, {
+
+        fetch(`${DBHelper.DATABASE_URL}reviews/`, {
             method: "POST",
             body: JSON.stringify(reviewSend)
-        })/*
-        fetch("http://localhost:1337/reviews", fetch_options)*/.then((response) => {
+        }).then((response) => {
             const contentType = response.headers.get("content-type");
             if (contentType && contentType.indexOf("application/json") !== -1) {
                 return response.json();
-            } else { return "API call successfull"}})
-            .then((data) => {console.log("Fetch successful!")})
+            } else {
+                return "API call successfull"
+            }
+        })
+            .then((data) => {
+                console.log("Fetch successful!")
+            })
             .catch(error => console.log("error:", error));
     }
 
-
-
     static sendDataWhenOnline(offline_obj) {
+        console.log("Offline OBJ", offline_obj);
         localStorage.setItem("data", JSON.stringify(offline_obj.data));
+        console.log(`Local Storage: ${offline_obj.object_type} stored`);
         window.addEventListener("online", (event) => {
+            console.log("Browser: Online again!");
             let data = JSON.parse(localStorage.getItem("data"));
+            console.log("updating and cleaning ui");
             [...document.querySelectorAll(".reviews_offline")]
                 .forEach(el => {
                     el.classList.remove("reviews_offline")
                     el.querySelector(".offline_label").remove()
                 });
             if (data !== null) {
+                console.log(data);
                 if (offline_obj.name === "addReview") {
                     DBHelper.addReview(offline_obj.data);
                 }
 
+                console.log("LocalState: data sent to api");
 
                 localStorage.removeItem("data");
+                console.log(`Local Storage: ${offline_obj.object_type} removed`);
             }
         });
     }
